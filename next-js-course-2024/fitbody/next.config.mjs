f9065a1bf6ce45522ebca635ffa7e610d220491f
jeweltutin <jeweltutin@gmail.com>
2024-08-08 10:43:00 +0600

    Parallax scroll gallery implemented with Aceternity UI

--- next-js-course-2024/fitbody/next.config.mjs
+++ next-js-course-2024/fitbody/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["drive.google.com", "https://imgbb.com/"],
+        domains: ["drive.google.com", "https://imgbb.com/", "images.unsplash.com"],
     },
 };
 
