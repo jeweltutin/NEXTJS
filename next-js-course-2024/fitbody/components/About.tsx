cdfd61e170bff05bfb4d2de346cd8a8e70d6f1db
jeweltutin <jeweltutin@gmail.com>
2024-08-20 13:30:25 +0600

    about achvmnt with motion anim

--- next-js-course-2024/fitbody/components/About.tsx
+++ next-js-course-2024/fitbody/components/About.tsx
@@ -1,9 +1,11 @@
+'use client';
 import { FaUsers } from "react-icons/fa";
 import { IoIosPricetag } from "react-icons/io";
 import { FaDumbbell } from "react-icons/fa";
 
 import { motion } from 'framer-motion';
 import { fadeIn } from "@/lib/variants";
+import Achievements from "./Achievements";
 
 const featured = [
   {
@@ -27,15 +29,30 @@ const About = () => {
   return (
     <section className="pt-8 pb-14 lg:pt-16 lg:pb-28" id='about'>
       <div className="container mx-auto">
-         <div className="flex flex-col items-center gap-2 mb-8">
-          <h2 className="h2 text-center">About</h2>
-          <p className="max-w-[600] mx-auto text-center">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, qui. Eligendi placeat, 
+        <div className="flex flex-col items-center gap-2 mb-8">
+          <motion.h2
+            variants={fadeIn('up', 0.4)}
+            initial="hidden"
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.2 }}
+            className="h2 text-center">About Us</motion.h2>
+          <motion.p
+            variants={fadeIn('up', 0.6)}
+            initial="hidden"
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.2 }}
+            className="max-w-[600] mx-auto text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, qui. Eligendi placeat,
             saepe eveniet quod aliquid ad incidunt sed sapiente quibusdam animi facere perspiciatis
-             nulla reiciendis ut expedita aspernatur vel.
-          </p>
-         </div>
-         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mb-14">
+            nulla reiciendis ut expedita aspernatur vel.
+          </motion.p>
+        </div>
+        <motion.div
+          variants={fadeIn('up', 0.8)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }}
+          className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mb-14">
           {featured.map((feature, index) => {
             return (
               <div key={index} className="flex flex-col justify-center items-center gap-4 border p-10">
@@ -43,17 +60,26 @@ const About = () => {
                   {feature.icon}
                 </div>
                 <div className="flex flex-col justify-center items-center gap-2 text-center">
-                  <h4 className="h4 text-accent">{ feature.title }</h4>
+                  <h4 className="h4 text-accent">{feature.title}</h4>
                   <p>
-                    { feature.subtitle }
+                    {feature.subtitle}
                   </p>
                 </div>
               </div>
             )
           })}
-         </div>
+        </motion.div>
+        <motion.div
+          variants={fadeIn('up', 1)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.2 }}
+          >
+          <Achievements />
+        </motion.div>
+
       </div>
-      
+
     </section>
   )
 }
--- next-js-course-2024/fitbody/components/Achievements.tsx
+++ next-js-course-2024/fitbody/components/Achievements.tsx
@@ -4,7 +4,7 @@ import { FaBriefcase, FaClock, FaTrophy } from "react-icons/fa";
 import { ImUsers } from "react-icons/im";
 import CountUp from "react-countup";
 import { useRef } from "react";
-import { useInView } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 
 const stats = [
     {
@@ -29,35 +29,70 @@ const stats = [
     }
 ]
 
+// Animation
+const stateContainerVariant = {
+    hidden: {
+        opacity: 0
+    },
+    show: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.4,
+            duration: 0.5,
+            ease: "linear"
+        }
+    }
+}
+
+const statsItem = {
+    hidden:{
+        y: 20,
+        opacity: 0
+    },
+    show: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            duration: 0.5,
+            ease: [0.25, 0.6, 0.3, 0.8]
+        }
+    }
+}
+
 const Achievements = () => {
     const ref = useRef(null);
     const isInView = useInView(ref);
     return (
         <section className="achievement-sec">
             <div className="container mx-auto py-14">
-                <div className="grid grid-cols-2 md:grid-cols-4 gap-16">
-                    {
-                        stats.map((item, index) => {
-                            return (
-                                <div key={index} className="flex flex-col justify-center items-center text-white">
-                                    <div className="border border-accent/90 w-[140px] h-[140px] mx-auto rounded-full p-1 mb-6">
-                                        <div ref={ref} className="border border-accent/30 w-full h-full flex items-center justify-center text-5xl rounded-full">
-                                            {isInView &&(
-                                                <CountUp start={0} end={item.number} duration={6} />
-                                            )} 
-                                        </div>
-                                    </div>
+                <motion.div 
+                  variants={stateContainerVariant}
+                  initial="hidden"
+                  whileInView={"show"}
+                  viewport={{ once: false, amount: 0.3 }}
+                  className="grid grid-cols-2 md:grid-cols-4 gap-16">
 
-                                    <div className="flex flex-col justify-center items-center text-center" >
-                                        <item.icon className="text-3xl mb-2" />
-                                        <h4>{ item.text }</h4>
+                    {stats.map((item, index) => {
+                        return (
+                            <motion.div 
+                              variants={statsItem}
+                              key={index} className="flex flex-col justify-center items-center text-white">
+                                <div className="border border-accent/90 w-[140px] h-[140px] mx-auto rounded-full p-1 mb-6">
+                                    <div ref={ref} className="border border-accent/30 w-full h-full flex items-center justify-center text-5xl rounded-full">
+                                        {isInView && (
+                                            <CountUp start={0} end={item.number} duration={6} />
+                                        )}
                                     </div>
-                                </div>)
-                        })
-                    }
-                </div>
+                                </div>
+
+                                <div className="flex flex-col justify-center items-center text-center" >
+                                    <item.icon className="text-3xl mb-2" />
+                                    <h4>{item.text}</h4>
+                                </div>
+                            </motion.div>)
+                    })}
+                </motion.div>
             </div>
-            Achievements
         </section>
     )
 }
