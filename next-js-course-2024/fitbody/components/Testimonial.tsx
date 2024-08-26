dd0959d8cd57d2537caed440a613ebfcc24bebfa
jeweltutin <jeweltutin@gmail.com>
2024-08-26 12:44:47 +0600

    fitbody swiper testimonial section with swiper auto slide

--- next-js-course-2024/fitbody/components/Testimonial.tsx
+++ next-js-course-2024/fitbody/components/Testimonial.tsx
@@ -1,8 +1,102 @@
- 
+"use client";
+import Image from "next/image";
+import { FaQuoteLeft } from "react-icons/fa6";
+import { Autoplay, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+
+
+
+const testimonialData = [
+  {
+    img: "/assets/img/testimonial/lucy.jpg",
+    message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quia ratione repellat laudantium, praesentium.",
+    name: "Lucy Anthony"
+  },
+  {
+    img: "/assets/img/testimonial/michael.jpg",
+    message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quia ratione repellat laudantium, praesentium.",
+    name: "Michael Smith"
+  },
+  {
+    img: "/assets/img/testimonial/maria.jpg",
+    message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quia ratione repellat laudantium, praesentium.",
+    name: "Maria Garcia"
+  },
+  {
+    img: "/assets/img/testimonial/lucy.jpg",
+    message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quia ratione repellat laudantium, praesentium.",
+    name: "Lucy Anthony"
+  },
+  {
+    img: "/assets/img/testimonial/michael.jpg",
+    message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quia ratione repellat laudantium, praesentium.",
+    name: "Michael Smith"
+  },
+  {
+    img: "/assets/img/testimonial/maria.jpg",
+    message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis quia ratione repellat laudantium, praesentium.",
+    name: "Maria Garcia"
+  }
+]
+
 const Testimonial = () => {
   return (
-    <section className="h-[50vh] bg-indigo-300" id="testimonial">
-      Testimonial
+    <section className="py-12 xl:py-28" id="testimonial">
+      <div className="container mx-auto">
+        <h2 className="h2 text-center">
+          Our Testimonials
+        </h2>
+        <div>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            modules={[Pagination, Autoplay]}
+            autoplay={{
+              delay: 3000,
+              pauseOnMouseEnter: true,
+              disableOnInteraction: false
+            }}
+            loop
+            pagination={{
+              clickable: true
+            }}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 30
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 30
+              }
+            }}
+            className="h-[320px]">
+            {testimonialData.map((person, index) => {
+              return (
+                <SwiperSlide key={index} className="h-full">
+                  <div className="flex flex-col justify-center items-center gap-6 text-center h-full">
+                    <Image src={person.img} width={90} height={90} alt="" className="rounded-full border-2 border-accent" />
+                    <div className="flex flex-col justify-center items-center">
+                      <FaQuoteLeft className="text-2xl text-gray-300" />
+                      <p className="max-w-[380px] mb-4">
+                        {person.message}
+                      </p>
+                      <span className="text-accent text-2xl">
+                        {person.name}
+                      </span>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              )
+            })}
+          </Swiper>
+        </div>
+
+      </div>
     </section>
   )
 }
