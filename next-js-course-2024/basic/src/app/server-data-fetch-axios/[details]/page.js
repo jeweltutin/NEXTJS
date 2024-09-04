6770b8e1aaae2fe6781ea5b8d5f981065b8af5fb
jeweltutin <jeweltutin@gmail.com>
2024-09-04 12:37:06 +0600

    new basic next app data show with axios

---
+++ next-js-course-2024/basic/src/app/server-data-fetch-axios/[details]/page.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+async function fetchUserDetails(currentUserId) {
+    try {
+        const { data } = await axios.get(`https://dummyjson.com/users/${currentUserId}`);        
+        return data;
+    } catch (e) {
+        throw new Error(e);
+    }
+}
+
+export default async function UserDetails({ params }) {
+    const userDetails = await fetchUserDetails(params.details);
+
+    return (
+        <div className="text-center pt-5">
+            <h1 className="text-4xl font-bold p-5 border-b-2 border-gray-300 mb-3">
+                This is user details page
+            </h1>
+            <div>
+                <p><strong>Name:</strong> {userDetails?.firstName} {userDetails?.lastName}</p>
+                <p><strong>Email:</strong> {userDetails?.email}</p>
+                <p><strong>Age:</strong> {userDetails?.age}</p>
+                <p><strong>Phone:</strong> {userDetails?.phone}</p>
+                <p><strong>Address:</strong> {userDetails?.address.address}, {userDetails?.address.city}, {userDetails?.address.country}</p>
+            </div>
+        </div>
+    )
+}
---
+++ next-js-course-2024/basic/src/app/server-data-fetch-axios/page.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import Link from "next/link";
+
+async function fetchListOfUsers() {
+    try {
+        const { data }   = await axios.get('https://dummyjson.com/users');
+        return data.users;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+export default async function ServerSideDataFetching() {
+    const listOfUsers = await fetchListOfUsers();
+    //console.log(listOfUsers);
+    return (
+        <div className="">
+            <h1 className="text-4xl font-bold text-center p-5 border-b-2 border-gray-300 mb-3">Server side data fetching - axios<br/><small>User List Page</small></h1>
+            <div className="flex justify-center">
+                {listOfUsers && listOfUsers.length > 0 ?
+                    <table className="w-full max-w-[950px] text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th className="px-6 py-3">Name</th>
+                                <th className="px-6 py-3">Email</th>
+                                <th className="px-6 py-3">Age</th>
+                                <th className="px-6 py-3">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {listOfUsers.map((user, index) => (
+                                <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                    <td className="px-6 py-3">{user.firstName} {user.lastName}</td>
+                                    <td className="px-6 py-3">{user.email}</td>
+                                    <td className="px-6 py-3">{user.age}</td>
+                                    <td className="px-6 py-3"><Link href={`/server-data-fetch-axios/${user.id}`} className="text-green-600">Details</Link></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                    : <h3> No user found </h3>
+                }
+            </div>
+        </div>
+    )
+}
