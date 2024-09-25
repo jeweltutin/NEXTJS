ead013d4474dcd710fc696a1959e7313f6beb4e8
jeweltutin <jeweltutin@gmail.com>
2024-09-25 17:54:38 +0600

    project grocery store with strapi backend

---
+++ next-js-course-2024/online-grocery-store/components/DropDown.jsx
@@ -0,0 +1,54 @@
+"use client";
+import GlobalApi from "@/actions/GlobalApi";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LayoutGrid } from "lucide-react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+function DropDown() {
+    const [categoryList, setCategoryList] = useState([]);
+
+    useEffect(() => {
+        getCategoryList();
+    }, [])
+
+    /**
+     * Get Category List
+     */
+    const getCategoryList = () => {
+        GlobalApi.getCategories().then(resp => {
+            console.log("CategoryList Resp:", resp);
+            setCategoryList(resp.data.data);
+        })
+    }
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <h2 className="hidden md:flex gap-2 items-center border rounded-full p-2 px-10 bg-slate-200 cursor-pointer">
+                    <LayoutGrid className="h-5 w-5" /> Category
+                </h2>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>Browse Category</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {categoryList.map((category, index) => (
+                    <DropdownMenuItem key={index} className="flex gap-4 items-center cursor-pointer">
+                        <Image src={process.env.NEXT_PUBLIC_BACKEND_BASE_URL + category?.icon[0]?.url} unoptimized={true} alt="icon" width={30} height={30} />
+                        <h4 className="text-lg">
+                            {category.name}
+                        </h4>
+                    </DropdownMenuItem>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+export default DropDown
---
+++ next-js-course-2024/online-grocery-store/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Search, ShoppingBag } from "lucide-react";
+import Image from "next/image"
+import { Button } from "./ui/button";
+import DropDown from "./DropDown";
+
+function Header() {
+    return (
+        <div className="flex p-5 shadow-md justify-between">
+            <div className="flex items-center gap-8">
+                <Image src="/images/logo.png" alt="logo" width={150} height={150} />
+                <DropDown />
+                <div className="md:flex hidden gap-3 items-center border rounded-full p-2 px-5 ">
+                    <Search />
+                    <input type="text" className="outline-none" placeholder="Search" />
+                </div>
+            </div>
+            <div className="flex gap-5 items-center">
+                <h4 className="flex gap-2 items-center"><ShoppingBag /> 0</h4>
+                <Button>Login</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
