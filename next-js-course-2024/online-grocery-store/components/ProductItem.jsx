a97609c13ce85e3024add952e7a3f9020630096a
jeweltutin <jeweltutin@gmail.com>
2024-10-31 10:22:00 +0600

    show % off in products

--- next-js-course-2024/online-grocery-store/components/ProductItem.jsx
+++ next-js-course-2024/online-grocery-store/components/ProductItem.jsx
@@ -17,8 +17,15 @@ import Link from 'next/link';
 function ProductItem({ product }) {
     const [openDialog, setOPenDialog] = useState(false);
 
+    let regularPrice = product.mrp;
+    let sellingPrice = product.sellingPrice;
+    let percentage;
+    if (sellingPrice) {
+        percentage = ((regularPrice - sellingPrice) / (regularPrice) * 100).toFixed(2); 
+    }
+    
     return (
-        <div className="p-2 md:p-6 flex flex-col items-center justify-center gap-3 border rounded-lg hover:scale-105 hover:shadow-lg transition-all ease-in-out cursor-pointer">
+        <div className="relative p-2 md:p-6 flex flex-col items-center justify-center gap-3 border rounded-lg hover:scale-105 hover:shadow-lg transition-all ease-in-out cursor-pointer">
             <Link href={"/product/" + product.slug}>
                 <div className="">
                     <Image src={process.env.NEXT_PUBLIC_BACKEND_BASE_URL + product?.images[0]?.url} width={500} height={200} alt={product.name} className="group-hover:scale-125 transition-all ease-in-out h-[200px] w-[200px] object-contain" />
@@ -28,10 +35,13 @@ function ProductItem({ product }) {
                     <div className="flex gap-3 items-center justify-center py-3">
                         {product.sellingPrice &&
                             <h3 className="font-bold text-lg text-center">{product.sellingPrice} Tk</h3>}
-                        <h3 className={`font-bold text-lg ${product.sellingPrice && 'line-through text-gray-500'}`}>{product.mrp} Tk</h3>
+                        <h3 className={`font-bold text-lg ${sellingPrice && 'line-through text-gray-500'}`}>{regularPrice} Tk</h3>
                     </div>
                 </div>
             </Link>
+            <div className="absolute text-sm right-0 top-2 text-white bg-primary px-[3px]">
+                {percentage && <span>Save {percentage}%</span>}
+            </div>
             <Button onClick={() => setOPenDialog(true)} variant="outline" className="text-primary hover:text-white hover:bg-primary">View</  Button>
             <Dialog open={openDialog} onOpenChange={() => {
                 setOPenDialog(false)
--- next-js-course-2024/online-grocery-store/components/ProductList.jsx
+++ next-js-course-2024/online-grocery-store/components/ProductList.jsx
@@ -8,8 +8,8 @@ function ProductList({ productList }) {
             <h2 className="text-green-600 font-bold text-2xl">
                 Our Popular Products
             </h2>
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-6">
-                {productList.map((product, index) => index < 8 && (
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-6">
+                {productList.map((product, index) => index < 10 && (
                     <ProductItem product={product} />
                 ))}
             </div>
